a3f82b377f58bd4b0967cb3d7bcc6149946d0b18
ca77y <ca77y@live.com>
2014-02-03 00:07:23 +0200

    test fixes

--- test/spec/treeview.spec.js
+++ test/spec/treeview.spec.js
@@ -40,34 +40,34 @@ describe('YaTreeviewCtrl', function () {
     }));
 
     it('should create a separate view', function () {
-        expect(scope.view).not.toBe(scope.model);
+        expect(scope.tree).not.toBe(scope.model);
     });
 
     it('should create a separate view with $model property on each node', function () {
-        expect(scope.view[0].$model).toBe(scope.model[0]);
-        expect(scope.view[2].$model).toBe(scope.model[2]);
-        expect(scope.view[0].$children[0].$model).toBe(scope.model[0].children[0]);
+        expect(scope.tree[0].$model).toBe(scope.model[0]);
+        expect(scope.tree[2].$model).toBe(scope.model[2]);
+        expect(scope.tree[0].$children[0].$model).toBe(scope.model[0].children[0]);
     });
 
     it('should create a separate view with $parent property on each node', function () {
-        expect(scope.view[0].$parent).toBeNull();
-        expect(scope.view[0].$children[0].$parent).toBe(scope.model[0]);
-        expect(scope.view[1].$children[0].$children[0].$parent).toBe(scope.model[1].children[0]);
+        expect(scope.tree[0].$parent).toBeNull();
+        expect(scope.tree[0].$children[0].$parent).toBe(scope.model[0]);
+        expect(scope.tree[1].$children[0].$children[0].$parent).toBe(scope.model[1].children[0]);
     });
 
     it('should create a separate view with $root property on each node', function () {
-        expect(scope.view[0].$root).toBeNull();
-        expect(scope.view[0].$children[0].$root).toBe(scope.model[0]);
-        expect(scope.view[1].$children[0].$children[0].$root).toBe(scope.model[1]);
+        expect(scope.tree[0].$root).toBeNull();
+        expect(scope.tree[0].$children[0].$root).toBe(scope.model[0]);
+        expect(scope.tree[1].$children[0].$children[0].$root).toBe(scope.model[1]);
     });
 
     it('should create a separate view with $children property on each node with children', function () {
-        expect(scope.view[0].$children.length).toBe(1);
-        expect(scope.view[0].$children[0].$children).toBeUndefined();
+        expect(scope.tree[0].$children.length).toBe(1);
+        expect(scope.tree[0].$children[0].$children).toBeUndefined();
     });
 
     it('should have collapsed property on each node', function () {
-        expect(scope.view[0].collapsed).toBeDefined();
-        expect(scope.view[0].$children[0].collapsed).toBeDefined();
+        expect(scope.tree[0].collapsed).toBeDefined();
+        expect(scope.tree[0].$children[0].collapsed).toBeDefined();
     });
 });
