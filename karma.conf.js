81de37d05aa3e20690beecc0aaa1df3393e7c424
ca77y <ca77y@live.com>
2014-02-09 12:04:10 +0100

    changing karma to phantomjs

--- karma.conf.js
+++ karma.conf.js
@@ -43,7 +43,7 @@ module.exports = function (config) {
         // - Safari (only Mac)
         // - PhantomJS
         // - IE (only Windows)
-        browsers: ['ChromeCanary'],
+        browsers: ['PhantomJS'],
 
 
         // Continuous Integration mode
